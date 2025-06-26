ea4be85f8dc7eace6b3263fba4d0dfd18521b612
Cassiano <cassiano.abreuu@gmail.com>
2025-06-26 08:04:01 -0300

    Initial commit

---
+++ Banco.java
@@ -0,0 +1,46 @@
+import java.util.ArrayList;
+
+
+class ContaBancaria {
+    public static void main(String[] args) {
+    private String numeroConta;
+    private double saldo;
+    private String titular;
+
+    public ContaBancaria(String numeroConta, String titular, double saldoInicial) {
+        this.numeroConta = numeroConta;
+        this.titular = titular;
+        this.saldo = saldoInicial;
+    }
+
+    public String getNumeroConta() {
+        return numeroConta;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public String getTitular() {
+        return titular;
+    }
+
+    public void depositar(double valor) {
+        if (valor > 0) {
+            saldo += valor;
+            System.out.println("Depósito de R$" + valor + " realizado com sucesso na conta " + numeroConta);
+        } else {
+            System.out.println("Valor de depósito inválido!");
+        }
+    }
+
+    public void sacar(double valor) {
+        if (valor > 0 && saldo >= valor) {
+            saldo -= valor;
+            System.out.println("Saque de R$" + valor + " realizado com sucesso na conta " + numeroConta);
+        } else {
+            System.out.println("Saldo insuficiente ou valor inválido para saque!");
+        }
+    }
+}
+}
